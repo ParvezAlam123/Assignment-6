a0e2133d5028c224218e795000567fb04111603a
ParvezAlam123 <62279025+ParvezAlam123@users.noreply.github.com>
2021-03-18 16:17:48 +0530

    Add files via upload

--- Code/Code_6.py
+++ Code/Code_6.py
@@ -1,10 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+
 
 X=[14,15,16,17,18,19,20,21]
 P=[0.133,0.0667,0.133,0.2,0.0667,0.133,0.2,0.0667]
-plt.stem(X,P,linefmt='green',label='theortical PMF')
+plt.stem(X,P,linefmt='green',label='Theortical PMF')
+
+x=[]
+P=[]
+for i in range(15):
+    x.append(random.randint(14,21))
+for i in range(8):
+    c=0
+    for j in range(15):
+        if x[j]==14+i:
+            c=c+1
+    prob=c*(1/15)
+    P.append(prob)
+plt.stem(X,P,linefmt='red',markerfmt='D',label='Experimental PMF')
 plt.xlabel('X')
 plt.ylabel('P(X=x)')
 plt.title('PMF of X')
-plt.show()
+plt.legend()
+plt.show()  
+  
+        
+    
